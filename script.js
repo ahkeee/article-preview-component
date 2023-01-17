914eb475d8116574b9c3f928a4347b3230b8ca03
Paolo Ren Ebueza <preebueza@gmail.com>
2023-01-17 13:56:48 +0800

    add mobile responsive design to the web site

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const btn = document.querySelector('.btn');
-const socialMedia = document.querySelector('.social-media')
+const socialMedia = document.querySelector('.social-media');
+const coloredSVG = document.querySelector('.colored-svg');
 
 btn.addEventListener('click', function() {
-        if (socialMedia.style.display !== "none") {
-          socialMedia.style.display = "none";
-        } else {
-          socialMedia.style.display = "block";
-        }
+  if (socialMedia.style.display !== "none") {
+    socialMedia.style.display = "none";
+    btn.classList.remove('hover');
+    coloredSVG.style.filter = '';
+  } else {
+    socialMedia.style.display = "block";
+    btn.classList.add('hover');
+    coloredSVG.style.filter = 'brightness(0) invert(1)';
+  }
 });
+
+
+
+
